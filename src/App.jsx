f1b3598136674f281c6bd32e973de8e1b508a278
Christopher Jimenez <jimenezchristopher5@gmail.com>
2024-08-31 13:04:24 -0500

    created top section of Collaboration component with checkmark svgs and titles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import ButtonGradient from './assets/svg/ButtonGradient';
 import Benefits from './components/Benefits';
+import Collaboration from './components/Collaboration';
 
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
       </div>
 
       <ButtonGradient />
